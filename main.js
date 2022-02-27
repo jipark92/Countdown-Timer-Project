e71ce285baaa81262a6a26b99f7dd8a99d1eecdb
ji <jiyongprk@gmail.com>
2022-02-27 18:51:28 -0500

    update

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
-img {
-    height: 50vh;
-}
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "Decenber"
+]
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+]
+
+const giveawayTime = document.querySelector('.giveaway');
+const countdownTime = document.querySelectorAll('.countdown');
+
+const date = new Date();
+
+console.log(date);
+
+const month = date.getMonth();
+
+console.log(`today month is ${months[month]}`);
+
+
+const day = date.getDay();
+
+console.log(days[day]);
+
+
