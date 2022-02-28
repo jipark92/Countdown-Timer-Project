8fd4b65664d5e090efe45df443fb0f77d752dbff
ji <jiyongprk@gmail.com>
2022-02-27 19:22:53 -0500

    getFutureDate function working

--- main.js
+++ main.js
@@ -26,14 +26,19 @@ const days = [
 const giveawayTime = document.querySelector('.giveaway');
 const countdownTime = document.querySelectorAll('.countdown');
 
+function getFutureDates () {
+    let futureDate = new Date(2022, 5, 12, 3, 30,0);
+    console.log(futureDate);
 
-let futureDate = new Date(2020, 4, 24, 11, 30,0);
+    let futureDateString = futureDate.toString();
+    console.log(futureDateString)
 
-let futureDateString = futureDate.toString();
+    let newFutureDateString = futureDateString.slice(0, -33);
+    console.log(newFutureDateString);
 
-let newFutureDateString = futureDateString.slice(0, -32);
+    giveawayTime.textContent += `${newFutureDateString}AM`;
 
-giveawayTime.textContent += newFutureDateString;
-
+}
+getFutureDates();
 
 
